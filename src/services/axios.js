150434a3d9afbfee8adb9fd233843acd139cf1f6
josephjose2243 <80537908+josephjose2243@users.noreply.github.com>
2025-05-07 23:53:36 +0530

    url imported from env file

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.BASE_URL 
+const baseURLfromenv = import.meta.env.BASE_URL 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: baseURLfromenv,
   headers: {
     "Content-Type": "application/json",
   },
