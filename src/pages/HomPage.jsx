8f96d6e5c8dc83a9be8dd03bb84bd4ceb49a313d
josephjose2243 <80537908+josephjose2243@users.noreply.github.com>
2025-05-08 02:18:49 +0530

    changes

--- src/pages/HomPage.jsx
+++ src/pages/HomPage.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
-const navigate = useNavigate();
+// const navigate = useNavigate();
 
-const handleNavigation = (path) => {
-    navigate(path);
-  };
+// const handleNavigation = (path) => {
+//     navigate(path);
+//   };
 
 const HomPage = () => {
   return (
     <div>
-       <button onClick={() => handleNavigation('/adminlogin')} > Admin Login</button>
-       <button  onClick={() => handleNavigation('/userlogin')}> User Login</button>
+      <h1>home page </h1>
+       {/* <button onClick={() => handleNavigation('/adminlogin')} > Admin Login</button>
+       <button  onClick={() => handleNavigation('/userlogin')}> User Login</button> */}
       <Link to="/adminlogin">admin Login1 </Link>;
     </div>
 
